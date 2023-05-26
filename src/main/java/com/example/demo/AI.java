21d33e80105db7180e27353afa2ed274c3f301d7
Yang <s0931861498@gmail.com>
2023-05-27 00:18:57 +0800

    AI version1

--- src/main/java/com/example/demo/AI.java
+++ src/main/java/com/example/demo/AI.java
@@ -2,31 +2,61 @@ package com.example.demo;
 
 public class AI {
     //全部都暫定，必須修正！！！
-    private int trafficflowEast;
-    private int trafficflowWest;
-    private int trafficflowNorth;
-    private int trafficflowSouth;
+    private int eastLane;
+    private int westLane;
+    private int northLane;
+    private int southLane;
 
-    private Yolov8 sensor = new Yolov8();
-    AI(Yolov8 sensor){
-        this.sensor = sensor;
-    }
-    public void getTrafficFlow(){
+    private double ratio;//each lane's compare value
 
+    //get the traffic flow from Yolov8
+    public int getTraffic() {
+        Yolov8 yolo = new Yolov8();
+        int lane = yolo.trafficFlow;
+        //assume yolov8 already calculated the traffic flow
+        //so created an instance of yolov8 then we can access the variable trafficFlow from yolov8
+        return lane;
     }
-    public int compareTraffic(int FlowA, int FlowB){
-        if (FlowA > FlowB){
-            return FlowA;
+
+    public double compareTrafficFlow(){
+        Yolov8 yoloE = new Yolov8();
+        int eastlane = yoloE.trafficFlow;
+
+        Yolov8 yoloW = new Yolov8();
+        int westlane = yoloW.trafficFlow;
+
+        Yolov8 yoloN = new Yolov8();
+        int northlane = yoloN.trafficFlow;
+
+        Yolov8 yoloS = new Yolov8();
+        int southlane = yoloS.trafficFlow;
+
+        //compare same direction's traffic flow
+        if(eastlane > westlane && northlane > southlane){
+            ratio = eastlane/northlane;
+        }else if(eastlane > westlane && northlane < southlane){
+            ratio = eastlane/southlane;
+        }else if(eastlane < westlane && northlane > southlane){
+            ratio = westlane/northlane;
         }else{
-            return FlowB;
+            ratio = westlane/southlane;
         }
-    }
-    public double compareTraffic(int FlowA, int FlowB, double radio){
-        if (FlowA > FlowB){
-            radio = FlowA / FlowB;
-        }else {
-            radio = FlowB / FlowA;
+
+        //depends on the ratio to determine traffic light time
+        if(ratio == 1 || (ratio > 0.66 && ratio < 1.5)){
+            System.out.println();//remain the same
+        }else if(ratio > 0.11 && ratio < 0.42){
+            //increase eastlane gren light time
+            //increase northlane red light time
+            System.out.println();
+        }else{
+            //decrease eastlane green light time
+            //decrease northlane red light time
+            System.out.println();
         }
-        return radio;
+        //note:the timing of traffic lights in the same way is linked
+        //which means EW is the same group, and NS too
+        System.out.println();
+        return ratio;
     }
 }
--- src/main/java/com/example/demo/Yolov8.java
+++ src/main/java/com/example/demo/Yolov8.java
@@ -1,4 +1,8 @@
 package com.example.demo;
 
 public class Yolov8 {
+    public int trafficFlow;
+    public void trafficFlow(){
+
+    }
 }
