babdf60c652310d07c293e550a1300a8cd1df987
Yang <s0931861498@gmail.com>
2023-05-29 21:44:45 +0800

    AI version2 Yolov8 update

--- src/main/java/com/example/demo/Yolov8.java
+++ src/main/java/com/example/demo/Yolov8.java
@@ -4,21 +4,25 @@ public class Yolov8 {
     public int trafficFlow;
     public int getData(String lane){
         if (lane == "East"){
-            System.out.println("File Loading...");
+            System.out.println("File of East Loading...");
 
             System.out.println("Loading success！！！");
+            //trafficFlow = 你抓取到的數值;
         }else if (lane == "West"){
-            System.out.println("File Loading...");
+            System.out.println("File of West Loading...");
 
             System.out.println("Loading success！！！");
+            //trafficFlow = 你抓取到的數值;
         }else if (lane == "North"){
-            System.out.println("File Loading...");
+            System.out.println("File of North Loading...");
 
             System.out.println("Loading success！！！");
+            //trafficFlow = 你抓取到的數值;
         }else if (lane == "South"){
-            System.out.println("File Loading...");
+            System.out.println("File of South Loading...");
 
             System.out.println("Loading success！！！");
+            //trafficFlow = 你抓取到的數值;
         }
         return trafficFlow;
     }
